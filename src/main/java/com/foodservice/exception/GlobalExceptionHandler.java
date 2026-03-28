62ffec25c7b61f32c511f95e5e1e83dc7c52baf2
AmanFedora <ap6262997@gmail.com>
2026-03-28 11:56:53 +0530

    (feat): methodArgumentTypeMissmatchException
    - modify to return exception in ErrorResponseDTO

--- src/main/java/com/foodservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/foodservice/exception/GlobalExceptionHandler.java
@@ -6,20 +6,35 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(OrderInvalidRequestException.class)
-    public ResponseEntity<ErrorResponseDTO> handleOrderInvalidRequestException(OrderInvalidRequestException ex, WebRequest webRequest) {
+    public ResponseEntity<ErrorResponseDTO> handleOrderInvalidRequestException(OrderInvalidRequestException orderInvalidRequestException, WebRequest webRequest) {
         ErrorResponseDTO errorResponse = new ErrorResponseDTO(
                 LocalDateTime.now(),
                 webRequest.getDescription(false),
-                HttpStatus.BAD_REQUEST,
-                ex.getMessage()
+                HttpStatus.NOT_FOUND,
+                orderInvalidRequestException.getMessage()
         );
 
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponseDTO> handleTypeMismatch (MethodArgumentTypeMismatchException methodArgumentTypeMismatchException, WebRequest webRequest) {
+
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
+                LocalDateTime.now(),
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                methodArgumentTypeMismatchException.getMessage()
+        );
+
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+
     }
 }
