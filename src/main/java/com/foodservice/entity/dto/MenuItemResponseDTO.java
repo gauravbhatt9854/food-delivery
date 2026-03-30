7b5a0eabe438c35068d580e7bc423016bff795f3
AmanKumar9122 <amanraj15112003@gmail.com>
2026-03-30 10:01:48 +0530

    refactor: update MenuItemResponseDTO fields and naming

--- src/main/java/com/foodservice/entity/dto/MenuItemResponseDTO.java
+++ src/main/java/com/foodservice/entity/dto/MenuItemResponseDTO.java
@@ -5,7 +5,9 @@ import java.math.BigDecimal;
 
 @Data
 public class MenuItemResponseDTO {
+    private Integer itemId;
     private String itemName;
     private String itemDescription;
     private BigDecimal itemPrice;
+    private Integer restaurantId;
 }
