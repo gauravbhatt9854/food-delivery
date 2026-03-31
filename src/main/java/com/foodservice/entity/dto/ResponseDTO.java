f3b8880fbbe4cc96ffe48efe56f9cd01e46cbf25
Samar Pratap Singh Wats <149253666+SamarWats@users.noreply.github.com>
2026-03-31 17:36:16 +0530

    Resolved conflict in ResponseDTO

--- src/main/java/com/foodservice/entity/dto/ResponseDTO.java
+++ src/main/java/com/foodservice/entity/dto/ResponseDTO.java
@@ -1,42 +1,45 @@
 package com.foodservice.entity.dto;
 
+import lombok.*;
 
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ResponseDTO {
     private Integer statusCode;
     private String message;
     private Object data;
-    public ResponseDTO() {}
-    
-        public ResponseDTO(int statusCode, String message, Object data) {
-            this.statusCode = statusCode;
-            this.message = message;
-            this.data = data;
-        }
-
-        public int getStatusCode() {
-            return statusCode;
-        }
-
-        public void setStatusCode(int statusCode) {
-            this.statusCode = statusCode;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-        }
-
-        public Object getData() {
-            return data;
-        }
-
-        public void setData(Object data) {
-            this.data = data;
-        }
+//    public ResponseDTO() {}
+//    
+//        public ResponseDTO(int statusCode, String message, Object data) {
+//            this.statusCode = statusCode;
+//            this.message = message;
+//            this.data = data;
+//        }
+//
+//        public int getStatusCode() {
+//            return statusCode;
+//        }
+//
+//        public void setStatusCode(int statusCode) {
+//            this.statusCode = statusCode;
+//        }
+//
+//        public String getMessage() {
+//            return message;
+//        }
+//
+//        public void setMessage(String message) {
+//            this.message = message;
+//        }
+//
+//        public Object getData() {
+//            return data;
+//        }
+//
+//        public void setData(Object data) {
+//            this.data = data;
+//        }
     
     
     
